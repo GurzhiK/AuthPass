c1e0c436379e04830989c5766d2a58c3cb5221be
GurzhiK <gurzhiy_pro@mail.ru>
2023-05-31 22:43:04 +0300

    first commit

--- client/src/pages/Containers/ResetPasswordConfirm.js
+++ client/src/pages/Containers/ResetPasswordConfirm.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { reset_password_confirm } from '../../actions/auth';
 
-const ResetPasswordConfirm = ({ match, reset_password }) => {
-  const navigate = useNavigate();
-
+const ResetPasswordConfirm = ({ reset_password_confirm }) => {
   const [requestSent, setRequestSent] = useState(false);
   const [formData, setFormData] = useState({
     new_password: '',
@@ -13,24 +11,23 @@ const ResetPasswordConfirm = ({ match, reset_password }) => {
   });
 
   const { new_password, re_new_password } = formData;
+  const navigate = useNavigate();
+  const { uid, token } = useParams();
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    reset_password_confirm(uid, token, new_password, re_new_password);
+    setRequestSent(true);
+  };
 
   useEffect(() => {
     if (requestSent) {
       navigate('/');
     }
-  }, [navigate]);
-
-  const onSubmit = e => {
-    e.preventDefault();
-
-    const uid = match.params.uid;
-    const token = match.params.token;
-
-    reset_password_confirm(uid, token, new_password, re_new_password);
-    setRequestSent(true);
-  };
+  }, [requestSent, navigate]);
 
   return (
     <div className="container px-[10vh] py-[8vh] min-h-[90vh] justify-around flex-wrap rounded-lg lg:px-14 lg:justify-between sm:justify-center sm:px-2 sm:grid-flow-row justify-items-center text-white">
